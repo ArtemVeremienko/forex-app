b204af3ae814951bbb188671f146821a0f3a5a9f
Artem <temaspsxxx@gmail.com>
2020-09-09 15:32:01 +0300

    feat: add Main components
    Main component has API for request data.
    And has 2 components Table and Converter.

---
+++ src/components/Main/Main.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import './Main.css';
+import Table from '../Table/Table'
+import Converter from '../Converter/Converter';
+
+const API = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
+
+export default function Main() {
+  const [requestCount, setRequestCount] = useState(() => localStorage.getItem('reqCounter') || 0);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(API).then(response => response.json());
+      setData(result);
+    };
+
+    fetchData();
+
+    setRequestCount(prev => {
+      localStorage.setItem('reqCounter', `${+prev + 1}`)
+      return +prev + 1;
+    })
+  }, [])
+
+  return (
+    <main className="main">
+      <Table data={data} isError={!(requestCount % 5)} />
+      <Converter />
+    </main>
+  )
+}
