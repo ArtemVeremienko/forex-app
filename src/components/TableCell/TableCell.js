ab830ac4ed695422593a1680763c86e9710da6d6
Artem <temaspsxxx@gmail.com>
2020-09-09 21:09:29 +0300

    feat: add edit and save value
    isHovering - show edit icon.
    isEditing - show save and close icons and delete edit icon.
    isSaving - show save icon when price in range +-10% from initial price.
    onChangePrice(newPrice) - call setData from Main.js.

--- src/components/TableCell/TableCell.js
+++ src/components/TableCell/TableCell.js
@@ -1,37 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './TableCell.module.css'
 
-export default function TableCell({ price }) {
-  const [hover, setHover] = useState(false);
+export default function TableCell({ price, onChangePrice }) {
+  const [value, setValue] = useState(price);
+  const [isHovering, setHovering] = useState(false);
+  const [isEditing, setEditing] = useState(false);
+  const [isSaving, setSaving] = useState(true);
 
-  const handleMouseEnter = () => {
-    setHover(true);
+  const inputRef = useRef();
+
+  const minPrice = price * 0.9;
+  const maxPrice = price * 1.1;
+
+  const handleClose = (isClose) => {
+    setHovering(isClose)
+    setEditing(false)
+    setValue(price);
   }
 
-  const handleMouseLeave = (event) => {
-    setHover(false)
-    event.target.contentEditable = false;
+  const handleEdit = () => {
+    setHovering(false);
+    setEditing(true);
+    inputRef.current.focus();
   }
 
-  const handleEdit = (event) => {
-    const cell = event.target.parentElement;
-    cell.contentEditable = true;
-    cell.focus();
-    setHover(false);
+  const handleChange = (event) => {
+    const inputValue = event.target.value;
+    if (inputValue < minPrice || inputValue > maxPrice) {
+      setSaving(false);
+    } else {
+      setSaving(true);
+    }
+    setValue(inputValue);
+  }
+
+  const handleSave = () => {
+    onChangePrice(value);
+    setEditing(false);
+    setHovering(true);
   }
 
   return (
     <td
       className={styles.cell}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={(event) => handleMouseLeave(event)}
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => handleClose(false)}
     >
-      {price}
-      {hover &&
+      <input
+        className={styles.input}
+        type="number"
+        value={value}
+        disabled={!isEditing}
+        onChange={handleChange}
+        ref={inputRef}
+        step="0.1"
+      />
+      {isHovering &&
         <button
           className={['material-icons', styles.button].join(' ')}
           onClick={handleEdit}
         >create</button>}
+      {isEditing && isSaving &&
+        <button
+          className={["material-icons", styles.button, styles.check].join(' ')}
+          contentEditable={false}
+          onClick={handleSave}
+        >check</button>
+      }
+      {isEditing &&
+        <button
+          className={["material-icons", styles.button].join(' ')}
+          contentEditable={false}
+          onClick={() => handleClose(true)}
+        > close</button >
+      }
     </td >
   )
 }
