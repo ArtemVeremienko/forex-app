1a42e7c440d998e66972c2e35c57486f18b1899c
Artem <temaspsxxx@gmail.com>
2020-09-09 15:34:33 +0300

    feat: add Table component
    Table component receive data and isError props.
    If isEror == true, show error.
    Else rendering table from data array.

---
+++ src/components/Table/Table.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import './Table.css';
+import TableRow from '../TableRow/TableRow';
+
+export default function Table({ data, isError }) {
+
+  return (
+    <>
+      {
+        isError ?
+
+          <h2 className="error">Server error <span role="img" aria-label="loudly crying face">😭</span></h2> :
+
+          <table className="main__table table">
+            <thead>
+              <tr>
+                <th>Currency/Current Date </th>
+                <th>Buy</th>
+                <th>Sell</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.map((item, index) => {
+                  const { ccy, base_ccy, buy, sale } = item;
+                  return <TableRow
+                    currency={`${ccy}/${base_ccy}`}
+                    buy={buy}
+                    sell={sale}
+                    key={index}
+                  />
+                })
+              }
+            </tbody>
+          </table>
+      }
+    </>
+  );
+}
