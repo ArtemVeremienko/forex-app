a98daad8c3181ffcefcb7a8bdecacd2b37a7e78b
Artem <temaspsxxx@gmail.com>
2020-09-10 14:40:41 +0300

    fix: add initial data state

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,18 +6,15 @@ import Converter from '../Converter/Converter';
 const API = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
 
 export default function Main() {
-  const [requestCount, setRequestCount] = useState(() => localStorage.getItem('reqCounter') || 0);
+  const [requestCount, setRequestCount] = useState(localStorage.getItem('reqCounter') || 0);
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([{ "ccy": "USD", "base_ccy": "UAH", "buy": "27.60000", "sale": "28.02000" }, { "ccy": "EUR", "base_ccy": "UAH", "buy": "32.40000", "sale": "33.01000" }, { "ccy": "RUR", "base_ccy": "UAH", "buy": "0.35500", "sale": "0.38200" }, { "ccy": "BTC", "base_ccy": "USD", "buy": "9779.1368", "sale": "10808.5196" }]);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await fetch(API)
         .then(response => response.json())
-        .catch(error => {
-          setRequestCount(5);
-          return [];
-        });
+        .catch(error => { setRequestCount(5); return []; });
       setData(result.filter(item => item.ccy !== 'RUR'));
     };
 
@@ -38,7 +35,7 @@ export default function Main() {
   return (
     <main className="main">
       <Table data={data} isError={!(requestCount % 5)} onChangePrice={onChangePrice} />
-      <Converter />
+      <Converter data={data} />
     </main>
   )
 }
