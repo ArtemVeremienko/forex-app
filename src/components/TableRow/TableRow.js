9f7963164b7dd44f01fb2954a96c42de9fc3b6b2
Artem <temaspsxxx@gmail.com>
2020-09-11 13:15:21 +0300

    refactor: add TableRow material components
    Material component TableRow change name to MuiTableRow.
    Add bold text style for first column.

--- src/components/TableRow/TableRow.js
+++ src/components/TableRow/TableRow.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import TableCell from '../TableCell/TableCell';
+import { makeStyles } from '@material-ui/core/styles';
+import MuiTableRow from '@material-ui/core/TableRow';
+import MuiTableCell from '@material-ui/core/TableCell';
+
+const useStyles = makeStyles({
+  firstCell: {
+    fontWeight: 'bold'
+  },
+});
 
 export default function TableRow({ currency, buy, sell, onChangePrice }) {
+  const classes = useStyles();
   return (
-    <tr>
-      <td>{currency}</td>
+    <MuiTableRow hover={true}>
+      <MuiTableCell className={classes.firstCell} align="center">{currency}</MuiTableCell>
       <TableCell price={+buy} onChangePrice={onChangePrice('buy')} />
       <TableCell price={+sell} onChangePrice={onChangePrice('sale')} />
-    </tr>
+    </MuiTableRow>
   )
 }
