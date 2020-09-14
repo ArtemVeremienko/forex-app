0fc75b004aa4c62faad114506bd2521518baf2f1
Artem <temaspsxxx@gmail.com>
2020-09-14 13:40:34 +0300

    refactor: add ConverterField components, delete select swap
    Create new component ConverterField input + select fields.
    Remove swap select values, becouse not enough data.

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,42 +1,36 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import ConverterField from '../ConverterField/ConverterField'
 import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 import SwapHoriz from '@material-ui/icons/SwapHoriz';
 
 
 export default function Converter({ data }) {
   const [exchange, setExchange] = useState({
-    base_ccy: 'UAH',
-    ccy: 'USD',
-    quote: 27.5,
+    from: 'UAH',
+    to: 'USD',
+    quote: 0,
     amount: '',
     result: 0,
-    changeList: ['UAH', "USD"],
-    getList: ['USD', 'EUR', 'BTC']
+    changeList: ['UAH'],
+    getList: ['USD']
   });
 
-  const [isSwap, setIsSwap] = useState(false);
-
-  const resultRound = (value, quote, swap = isSwap) => {
-    return swap ?
-      Math.round(value * quote * 100) / 100 :
-      Math.round(value / quote * 100) / 100;
-  }
+  const resultRound = (value, quote) => (value / quote).toFixed(3);
 
   useEffect(() => {
     setExchange(prev => {
-      const newList = data.filter(i => i.base_ccy === prev.base_ccy);
+      const newList = data.filter(i => i.base_ccy === prev.from);
       const quote = newList[0].buy
       return {
         ...prev,
-        result: resultRound(prev.amount, quote),
         quote: quote,
+        result: +resultRound(prev.amount, quote) || '',
+        changeList: [...new Set(data.map(i => i.base_ccy))],
         getList: newList.map(i => i.ccy),
       }
     })
-  }, [])
+  }, [data])
 
   const handleChangeInput = (e) => {
     const value = e.target.value;
@@ -48,98 +42,81 @@ export default function Converter({ data }) {
   }
 
   const handleChangeSelect = (e) => {
-    const value = e.target.value;
+    const currency = e.target.value;
     setExchange(prev => {
-      const newGetList = data.filter(i => i.base_ccy === value);
+      const newGetList = data.filter(i => i.base_ccy === currency);
       const quote = newGetList[0].buy
       return {
         ...prev,
-        base_ccy: value,
-        quote: quote,
+        quote,
+        from: currency,
         result: resultRound(prev.amount, quote),
         getList: newGetList.map(i => i.ccy),
       }
     })
   }
 
-  const handleGetSelect = (e) => {
+  const handleGetInput = (e) => {
     const value = e.target.value;
+    setExchange(prev => ({
+      ...prev,
+      result: value,
+      amount: value * prev.quote,
+    }))
+  }
+
+  const handleGetSelect = (e) => {
+    const currency = e.target.value;
     setExchange(prev => {
-      const newList = data.filter(i => i.ccy === value);
-      const quote = newList[0].buy;
+      const newChangeList = data.filter(i => i.ccy === currency);
+      const quote = newChangeList[0].buy;
       return {
         ...prev,
-        ccy: value,
-        result: resultRound(prev.amount, quote),
         quote,
+        to: currency,
+        result: resultRound(prev.amount, quote),
       }
     })
   }
 
+
+
   const swapValue = () => {
-    setIsSwap(!isSwap);
     setExchange(prev => ({
       ...prev,
       amount: prev.result,
       result: prev.amount,
-      ccy: prev.base_ccy,
-      base_ccy: prev.ccy,
-      changeList: prev.getList,
-      getList: prev.changeList,
     }))
   }
 
   return (
     <Box mx="auto" display="flex" justifyContent="center" mt={3} alignItems="center">
-      <Box mr={3} display="flex" flexWrap="wrap" justifyContent="flex-end">
-        <TextField
-          label="Change"
-          variant="outlined"
-          type="number"
-          min="0"
-          value={exchange.amount}
-          onChange={handleChangeInput}
-        />
 
-        <TextField
-          value={exchange.base_ccy}
-          onChange={handleChangeSelect}
-          select
-          variant="outlined"
+      <ConverterField
+        label="Change"
+        inputValue={exchange.amount}
+        handleInput={handleChangeInput}
+        selectValue={exchange.from}
+        handleSelect={handleChangeSelect}
+        optionList={exchange.changeList}
+      />
+
+      <Box mx={3}>
+        <Button variant="contained" color="primary"
+          onClick={swapValue} size="large" startIcon={<SwapHoriz />}
         >
-          {exchange.changeList.map((cur, i) =>
-            <MenuItem value={cur} key={i}>{cur}</MenuItem>
-          )}
-        </TextField>
+          Swap
+        </Button>
       </Box>
 
-      <Button variant="contained" color="primary"
-        onClick={swapValue} size="large" startIcon={<SwapHoriz />}
-      >
-        Swap
-      </Button>
-
-      <Box ml={3} display="flex" flexWrap="wrap">
-        <TextField
-          label="Get"
-          variant="outlined"
-          type="number"
-          min="0"
-          value={exchange.result}
-          readOnly={true}
-        />
-
-        <TextField
-          value={exchange.ccy}
-          onChange={handleGetSelect}
-          select
-          variant="outlined"
-        >
-          {exchange.getList.map((cur, i) =>
-            <MenuItem value={cur} key={i}>{cur}</MenuItem>
-          )}
-        </TextField>
-      </Box>
+      <ConverterField
+        label="Get"
+        inputValue={exchange.result}
+        handleInput={handleGetInput}
+        selectValue={exchange.to}
+        handleSelect={handleGetSelect}
+        optionList={exchange.getList}
+      />
     </Box >
   )
 }
